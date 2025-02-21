cb5faf83624fbff4c1fd9a109d50a1fda9ab85c3
Jagger <>
2025-02-21 14:05:00 +0800

    没啥说的

---
+++ 复试编程题/一元二次方程求根.cpp
@@ -0,0 +1,39 @@
+//
+// Created by jagger on 25-2-19.
+//
+/*
+一元二次方程求根：编程求一元二次方程$ax^2 + bx + c = 0$的根$x_1$和$x_2$。(a!=0)
+ */
+#include <iostream>
+#include <math.h>
+#include <valarray>
+
+int main()
+{
+    float a, b, c, x1, x2;
+    scanf("%f %f %f", &a, &b, &c);
+    // 先用判别式确定有几个实根b^2-4a*c
+    // b*b-4*a*c
+    float tmp = b * b - 4 * a * c;
+    if (a == 0)
+    {
+        printf("非一元二次方程");
+        return 0;
+    }
+    if (tmp < 0) //共轭复根 a+-bi
+    {
+        printf("No solution");
+        return 0;
+    }
+    if (tmp == 0)
+    {
+        x1 = (-b + sqrt(tmp)) / (2 * a);
+        x2 = x1;
+        printf("x1:%f x2:%f\n", x1, x2);
+        return 0;
+    }
+    x1 = (-b + sqrt(tmp)) / (2 * a);
+    x2 = (-b - sqrt(tmp)) / (2 * a);
+    printf("x1:%f x2:%f\n", x1, x2);
+    return 0;
+}
